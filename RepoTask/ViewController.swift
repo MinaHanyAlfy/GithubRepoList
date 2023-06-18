5544edb89fcbba4c950d3ed6ed1f2c1072f666de
Mina Hany <10minahany10@gmail.com>
2023-06-18 21:14:50 +0300

    First Commit & Removing Scene Delegate.

---
+++ RepoTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RepoTask
+//
+//  Created by John Doe on 2023-06-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
